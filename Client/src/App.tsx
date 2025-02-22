32f6d910229954919f078bc120b020ce1b2d924f
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-02-22 23:16:12 +0600

    Database Connection done

---
+++ Client/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className=" text-3xl font-bold underline">hi guys</p>
+    </>
+  );
+}
+
+export default App;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import "dotenv/config";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 app.use(express.json())
 app.use(cors())
+
+mongoose.connect(process.env.MONGO_URL as string, {
+    serverSelectionTimeoutMS: 50000, 
+  })
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.error("Database connection error:", err));
+
 
 app.get("/test" , async (req: Request, res: Response)=>{
     res.json({message: "hello!"});
