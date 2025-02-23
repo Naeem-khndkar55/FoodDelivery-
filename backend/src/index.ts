6c722d6d6c95ec552193c5b32ce7ba8b6b208675
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-02-23 19:08:30 +0600

    Create user api created

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,21 +2,17 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import MyUserRoute from "./routes/myUserRoute"
 dotenv.config();
 const app = express();
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URL as string, {
-    serverSelectionTimeoutMS: 50000, 
-  })
-  .then(() => console.log("Database connected"))
+mongoose.connect(process.env.MONGO_URL as string).then(() => console.log("Database connected"))
   .catch((err) => console.error("Database connection error:", err));
 
-
-app.get("/test" , async (req: Request, res: Response)=>{
-    res.json({message: "hello!"});
-});
+//routers
+app.use("/api/my/user",MyUserRoute);
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
